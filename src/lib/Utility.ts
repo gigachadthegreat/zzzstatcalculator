1e5178f3459315afe019bb4e27bbe1a61bd00237
gigachadthegreat <gigachadgaminggroove@gmail.com>
2025-10-13 17:32:09 +0200

    fix undo last fix

--- src/lib/Utility.ts
+++ src/lib/Utility.ts
@@ -95,7 +95,7 @@ export const getParameterizedStatsAsUrl = (
 
 export const getsettingsFromUrl = () => {
     const params = new URLSearchParams(window.location.search);
-    const settings: { [key: string]: string } = {};
+    const settings: { [key: string]: any } = {};
 
     const parseParam = (key: string, type: "string" | "number" | "boolean" | "json") => {
         const value = params.get(key);
